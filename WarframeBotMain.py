87e75a54688916e53d258855231e03d209e5e9c4
CatFailure <45643657+CatFailure@users.noreply.github.com>
2018-12-24 21:26:20 +0000

    Update WarframeBotMain.py

--- WarframeBotMain.py
+++ WarframeBotMain.py
@@ -9,7 +9,6 @@ import querying_html as query_html
 import querying_weapons as query_weap
 
 # Bot token
-TOKEN = "NDc2MTMwMzc2ODQzMTMyOTQ5.Dus8Gg.NWW1YN279fFIbfk__TT6UOxHzEM"
 
 # Define a prefix
 BOT_Prefix = "="
